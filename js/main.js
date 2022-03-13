89a3c544b2536e6f7b32c5cd52d5c54f27daf90e
Henrique Santos <hv_santos@outlook.com>
2022-03-13 13:14:12 -0300

    add a function 'oneRun()" to show all films in the categories, add a new option to the select list

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const $select = document.querySelector('#select-value')
 $select.addEventListener('input', verifyCategory)
 const $card = document.querySelector('.film-list')
-
+oneRun()
 function verifyCategory() {
   let category = $select.value
   if (category == 'scifi') {
@@ -43,5 +43,37 @@ function verifyCategory() {
         <img src="${filmsAdventure[counterAdventure].img}" alt=""/>
       </div>`
     }
+  }
+}
+
+function oneRun() {
+  for (let counterScifi = 0; counterScifi < filmsScifi.length; counterScifi++) {
+    $card.innerHTML += `
+    <div class="card">
+      <p class="main-subtitle -filmtitle">${filmsScifi[counterScifi].name}</p>
+      <img src="${filmsScifi[counterScifi].img}" alt=""/>
+    </div>`
+  }
+  for (
+    let counterComedy = 0;
+    counterComedy < filmsComedy.length;
+    counterComedy++
+  ) {
+    $card.innerHTML += `
+    <div class="card">
+      <p class="main-subtitle -filmtitle">${filmsComedy[counterComedy].name}</p>
+      <img src="${filmsComedy[counterComedy].img}" alt=""/>
+    </div>`
+  }
+  for (
+    let counterAdventure = 0;
+    counterAdventure < filmsAdventure.length;
+    counterAdventure++
+  ) {
+    $card.innerHTML += `
+    <div class="card">
+      <p class="main-subtitle -filmtitle">${filmsAdventure[counterAdventure].name}</p>
+      <img src="${filmsAdventure[counterAdventure].img}" alt=""/>
+    </div>`
   }
 }
